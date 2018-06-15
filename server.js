6aa3df9ab9b108113a21872773ba40c5d9ac29d4
matthewvolk <volkmattj@gmail.com>
2018-06-14 22:49:29 -0700

    add static assets and view engine in server.js, add bootstrap theme in public/css, add index.ejs

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 
-// VIEWS are not static
-// STATIC is static
+// static is static
+app.use('/static', express.static('public'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// views 
+app.set('view engine', 'ejs');
 
+// root
+app.get('/', (req, res) => (
+  res.render('pages/index')
+));
+
+// server
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
