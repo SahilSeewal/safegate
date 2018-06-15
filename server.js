cc4c13b3df6d8fb2d7f2b685fb05922dc17d4bdc
matthewvolk <volkmattj@gmail.com>
2018-06-14 22:13:10 -0700

    install ejs

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// VIEWS are not static
+// STATIC is static
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
