5bede6f972b8e266b03d648441612abcfb56c766
matthewvolk <volkmattj@gmail.com>
2018-06-17 18:31:12 -0700

    add mongodb database connection

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // Init
 const express = require('express');
+const mongoose = require('mongoose');
+const configDB = require('./config/database.js');
+
 const app = express();
 
 // https://scotch.io/tutorials/easy-node-authentication-setup-and-local 
@@ -7,6 +10,18 @@ const app = express();
 // is the flexibility you obtain when using it. No rigid schema. If three 
 // months down the line you need a certain table to have an extra field, 
 // and this and that, you just change it and it ripples out from there on out.
+
+// Database
+mongoose.connect(configDB.url);
+let db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+db.on('error', err => {
+  console.log(err)
+});
 
 // Views 
 app.set('view engine', 'ejs');
