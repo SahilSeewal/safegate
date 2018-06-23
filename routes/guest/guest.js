92501c01a3ef1bdbe649a5ed0cac3b3930a9c52e
matthewvolk <volkmattj@gmail.com>
2018-06-22 22:43:06 -0700

    add guest registration POST route

--- routes/guest/guest.js
+++ routes/guest/guest.js
@@ -9,4 +9,8 @@ router.get('/signup', function(req, res) {
   res.render('pages/guest/signup')
 })
 
+router.post('/register', function(req, res) {
+  res.send('Guest Registered!')
+})
+
 module.exports = router;
