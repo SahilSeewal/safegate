c2e42123fd639c563e6196a485c7f659384d3ae3
matthewvolk <volkmattj@gmail.com>
2018-06-24 12:51:24 -0700

    add TODO comments

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const dbConfig = require('./config/database.js');
 
 /**
  * TODO
- * [X] Finish GuestSchema to take email and password
- * [X] Create views for guest signup
  * [] Install passport
  * [] Install passport-local
  * [] http://www.passportjs.org/docs/configure/
@@ -39,7 +37,7 @@ mongoose.connection.on('error', dbConfig.error);
 
 // Server
 // https://www.youtube.com/watch?v=XxRuW1pfGTI&t=1s <- Find Similar
-const port = process.env.port || 3000;
+const port = process.env.port || 3000; // TODO: configure this to read from $PORT
 app.listen(port, () => {
   console.log(`SafeGate server running on http://localhost:${port}/`);
 });
