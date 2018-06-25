537b741c2b9cecf029f3ac4e6984dae0f7dee32a
matthewvolk <volkmattj@gmail.com>
2018-06-24 20:58:09 -0700

    added passport and required in server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express    = require('express')
 const mongoose   = require('mongoose');
 const morgan     = require('morgan');
 const bodyParser = require('body-parser');
+const passport   = require('passport');
 
 
 // Initialize Express
