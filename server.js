ef0dd952041183e2a3d43d42a468ba00054443c7
matthewvolk <volkmattj@gmail.com>
2018-06-24 21:03:50 -0700

    add connect-flash and express-session packages, require and configure them in server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // Require Modules
 const path       = require('path');
-const express    = require('express')
+const express    = require('express');
+const session    = require('express-session');
 const mongoose   = require('mongoose');
 const morgan     = require('morgan');
 const bodyParser = require('body-parser');
 const passport   = require('passport');
+const flash      = require('connect-flash');
 
 // Initialize Express
 const app      = express();
@@ -43,7 +45,10 @@ mongoose.connection.once('open', dbConfig.open);
 mongoose.connection.on('error', dbConfig.error);
 
 // Required for Passport:
+app.use(session({ secret: 'thisshoulddefinitelybechanged' }));
 app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 // Routes
 app.use('/', require('./routes'));
