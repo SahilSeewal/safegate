33e1285bfeafea0fe2de6684299f2d3c5792ec72
matthewvolk <volkmattj@gmail.com>
2018-06-29 07:53:41 -0700

    require Guest model in guest controller, change successRedirect to guest dashboard

--- controllers/guest/guest.js
+++ controllers/guest/guest.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const passport = require('passport');
+const Guest = require('../../models/guest');
 
 function index(req, res) {
-  res.send('Guest Dashboard')
+  res.send('Guest Dashboard');
 }
 
 function getSignup(req, res) {
@@ -16,7 +17,7 @@ function getSignup(req, res) {
 
 function postSignup(req, res, next) {
   passport.authenticate('local-guest-signup', {
-    successRedirect : '/guest/signup', // redirect to the secure profile section
+    successRedirect : '/guest/', // redirect to the secure profile section
     failureRedirect : '/guest/signup', // redirect back to the signup page if there is an error
     failureFlash : true // allow flash messages
   })(req, res, next)
