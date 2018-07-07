174caed22b421df9a7b336e6cfec87cc56260598
matthewvolk <volkmattj@gmail.com>
2018-07-07 16:32:15 -0700

    WIP: add route authentication middleware for guests

--- controllers/guest/guest.js
+++ controllers/guest/guest.js
@@ -52,6 +52,21 @@ function getUser(req, res) {
   res.send(req.user);
 }
 
+/** 
+ * Route Authentication Middleware
+ * This function should be used to protect all private Guest routes
+ * from users who are not logged in as a guest. I need to learn more
+ * about redirects before finishing this.
+ */
+function authenticateLocalGuest(req, res, next) {
+  if (!req.user)
+    res.redirect('/')
+  if (req.user.userGroup !== "Guest")
+    res.redirect('back')
+  if (req.user.userGroup === "Guest")
+    next();
+}
+
 // Export Controllers
 module.exports = {
   index: index,
@@ -60,5 +75,5 @@ module.exports = {
   getLogin: getLogin,
   postLogin: postLogin,
   getLogout: getLogout,
-  getUser: getUser
+  getUser: getUser,
 }
