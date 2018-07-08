47306b85e5e101a48922229a358e6b970777be29
matthewvolk <volkmattj@gmail.com>
2018-07-07 20:26:18 -0700

    create local passport strategies for resident signup and login, import strategy and model into resident controller

--- controllers/resident/resident.js
+++ controllers/resident/resident.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const passport = require('passport');
+const Guest = require('../../models/guest');
 
 function index(req, res) {
   res.send('Resident Dashboard')
