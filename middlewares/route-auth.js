9af4eca338a83eaaff3d444067bfb32237708b41
matthewvolk <volkmattj@gmail.com>
2018-07-16 06:32:20 -0700

    fixed comments

--- middlewares/route-auth.js
+++ middlewares/route-auth.js
@@ -4,10 +4,12 @@
  * [] Users who are not logged in should not access Guest or Resident private routes
  * [] Guests who are logged in should not access Resident private routes
  * [] Residents who are logged in should not access Guest private routes
+ * 
  */
+
 function redirectUserIfNotLoggedIn(req, res, next) {
   if (!req.user)
-    res.redirect('back');
+    res.redirect('back'); 
   next();
 }
 
